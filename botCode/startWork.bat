cca6ab1e473b36f05e4b92909b8c5aea365072c8
machnev_egor <46050097+machnevegor@users.noreply.github.com>
2021-01-01 17:30:05 +0300

    :/

---
+++ botCode/startWork.bat
@@ -0,0 +1,16 @@
+@: Authors of the project:
+@: 1-MachnevEgor_https://vk.com/machnev_egor
+@: 2-DmitryShalimov_https://vk.com/lookatstat
+@: Contacts in email:
+@: 1-meb.official.com@gmail.com
+@: 2-dmitriy-shalimov@yandex.ru
+
+python MainBotLogic.py --user
+pause
+
+@: Authors of the project:
+@: 1-MachnevEgor_https://vk.com/machnev_egor
+@: 2-DmitryShalimov_https://vk.com/lookatstat
+@: Contacts in email:
+@: 1-meb.official.com@gmail.com
+@: 2-dmitriy-shalimov@yandex.ru
